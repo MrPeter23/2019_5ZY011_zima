25ced426664cc81300c019c632cc025964de0d43
Jan Janech <jan.janech@fri.uniza.sk>
2019-10-09 09:13:12 +0200

    03a - spravna implementacia mrc

--- kalkulacka/kalkulacka/Kalkulacka.java
+++ kalkulacka/kalkulacka/Kalkulacka.java
@@ -1,26 +1,32 @@
 public class Kalkulacka {
     private double medzivysledok;
     private double pamat;
+    private boolean mrcStlacene;
     
     public Kalkulacka(double prveCislo) {
         this.medzivysledok = prveCislo;
         this.pamat = 0;
+        this.mrcStlacene = false;
     }
     
     public void scitaj(double operand) {
         this.medzivysledok += operand;
+        this.mrcStlacene = false;
     }
     
     public void odpocitaj(double operand) {
         this.medzivysledok -= operand;
+        this.mrcStlacene = false;
     }
     
     public void vynasob(double operand) {
         this.medzivysledok *= operand;
+        this.mrcStlacene = false;
     }
     
     public void vydel(double operand) {
         this.medzivysledok /= operand;
+        this.mrcStlacene = false;
     }
     
     public double getVysledok() {
@@ -28,26 +34,32 @@ public class Kalkulacka {
     }
     
     public void mRC() {
-        if (this.medzivysledok != this.pamat) {
-            this.medzivysledok = this.pamat;
-        } else {
+        if (this.mrcStlacene) {
             this.pamat = 0;
+            this.mrcStlacene = false;
+        } else {
+            this.medzivysledok = this.pamat;
+            this.mrcStlacene = true;
         }
     }
     
     public void mC() {
         this.pamat = 0;
+        this.mrcStlacene = false;
     }
     
     public void mR() {
         this.medzivysledok = this.pamat;
+        this.mrcStlacene = false;
     }
     
     public void mPlus() {
         this.pamat += this.medzivysledok;
+        this.mrcStlacene = false;
     }
     
     public void mS() {
         this.pamat = this.medzivysledok;
+        this.mrcStlacene = false;
     }
 }

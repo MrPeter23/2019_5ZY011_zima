9f390d1063bc8e56feb5ddcc6b7f19a093ba9909
Jan Janech <jan.janech@fri.uniza.sk>
2019-10-16 08:52:22 +0200

    04a - spravne umiestnenie segmentu

--- segmentovyDisplej/Segment.java
+++ segmentovyDisplej/Segment.java
@@ -1,7 +1,10 @@
 public class Segment {
     private Obdlznik zobrazenie;
     
-    public Segment() {
+    public Segment(int dlzkaSegmentu, int hrubkaSegmentu, int poziciaX, int poziciaY) {
         this.zobrazenie = new Obdlznik();
+        this.zobrazenie.zmenStrany(dlzkaSegmentu, hrubkaSegmentu);
+        this.zobrazenie.posunVodorovne(poziciaX - 60);
+        this.zobrazenie.posunZvisle(poziciaY - 50);
     }
 }

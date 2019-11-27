d98b1fdbd01a87f0dcc7e509fab2894867133dd4
Jan Janech <jan.janech@fri.uniza.sk>
2019-11-27 09:14:53 +0100

    10a - kontroly tahu

--- nim/Nim.java
+++ nim/Nim.java
@@ -20,15 +20,36 @@ public class Nim {
     }
     
     public void posunDole(int oKolko) {
+        if (oKolko <= 0) {
+            System.out.println("Musis tahat o kladny pocet policok");
+            return;
+        }
+        
         int x = this.kamen.getX();
         int y = this.kamen.getY() - oKolko;
+        
+        if (y < 0) {
+            System.out.println("Snazis sa vybehnut zo sachovnice");
+            return;
+        }
+        
         this.kamen.posunKamen(x, y);
         this.prvyNaTahu = !this.prvyNaTahu;
     }
     
     public void posunVlavo(int oKolko) {
+        if (oKolko <= 0) {
+            System.out.println("Musis tahat o kladny pocet policok");
+            return;
+        }
+        
         int x = this.kamen.getX() - oKolko;
         int y = this.kamen.getY();
+        
+        if (x < 0) {
+            System.out.println("Snazis sa vybehnut zo sachovnice");
+            return;
+        }
         this.kamen.posunKamen(x, y);
         this.prvyNaTahu = !this.prvyNaTahu;
     }

55c7fb938c0997b54ab9cf74e8e42843d5a4a91b
Jan Janech <jan.janech@fri.uniza.sk>
2019-11-27 09:07:39 +0100

    10a - hrac na tahu

--- nim/Nim.java
+++ nim/Nim.java
@@ -1,21 +1,35 @@
 public class Nim {
     private Kamen kamen;
+    private String[] menaHracov;
+    private boolean prvyNaTahu;
     
-    public Nim(int sirka, int vyska) {
+    public Nim(int sirka, int vyska, String menoPrveho, String menoDruheho) {
         Sachovnica sachovnica = new Sachovnica(sirka, vyska);
         this.kamen = sachovnica.vlozKamen();
         sachovnica.zobraz();
+        this.menaHracov = new String[] {menoPrveho, menoDruheho};
+        this.prvyNaTahu = true;
+    }
+    
+    public String getHracNaTahu() {
+        if (this.prvyNaTahu) {
+            return this.menaHracov[0];
+        } else {
+            return this.menaHracov[1];
+        }
     }
     
     public void posunDole(int oKolko) {
         int x = this.kamen.getX();
         int y = this.kamen.getY() - oKolko;
         this.kamen.posunKamen(x, y);
+        this.prvyNaTahu = !this.prvyNaTahu;
     }
     
     public void posunVlavo(int oKolko) {
         int x = this.kamen.getX() - oKolko;
         int y = this.kamen.getY();
         this.kamen.posunKamen(x, y);
+        this.prvyNaTahu = !this.prvyNaTahu;
     }
 }
